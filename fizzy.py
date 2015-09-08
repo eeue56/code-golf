174f5f8db861dc9860af9bea19d4cddd7c061370
eeue56 <eeue56@users.noreply.github.com>
2015-09-08 18:03:39 +0100

    Delete fizzy.py

--- fizzy.py
+++
@@ -1,9 +0,0 @@
-def w(k):
-    o = ''
-    while k:
-        m = lambda x : k/x == (k + 0.0)/x
-        l = 'Fizz'
-        b = 'Buzz'
-        o = [`k`, [b, l][m(3)], l+b][m(3) + m(5)] + '\n' + o
-        k -= 1
-    return o
